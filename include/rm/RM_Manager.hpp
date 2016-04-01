37b98472fd8cf6435b1b5f44007d8703137451c2
Ngoguey42 <ngoguey@student.42.fr>
2016-04-01 07:02:09 +0200

    Clean comments in RM_Manager.hpp

--- include/rm/RM_Manager.hpp
+++ include/rm/RM_Manager.hpp
@@ -6,14 +6,12 @@
 //   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2016/03/30 14:50:39 by ngoguey           #+#    #+#             //
-//   Updated: 2016/04/01 06:41:18 by ngoguey          ###   ########.fr       //
+//   Updated: 2016/04/01 07:01:22 by ngoguey          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 #ifndef RM_MANAGER_HPP
 # define RM_MANAGER_HPP
-
-// # include <>
 
 # include "pf/pf.h"
 
@@ -42,18 +40,42 @@ public:
 	RM_Manager &operator=(RM_Manager &&rhs) = delete;
 
 	/* EXPOSED ********************** */
-	// This method should call PF_Manager::CreateFile to create a paged file called fileName. The records in this file will all have size recordSize. This method should initialize the file by storing appropriate information in the header page. Although recordSize will usually be much smaller than the size of a page, you should compare recordSize with PF_PAGE_SIZE and return a nonzero code if recordSize is too large for your RM component to handle.
+	/** This method should call PF_Manager::CreateFile to create a paged file
+	 ** called fileName. The records in this file will all have size recordSize.
+	 ** This method should initialize the file by storing appropriate informa-
+	 ** tion in the header page. Although recordSize will usually be much sma-
+	 ** ller than the size of a page, you should compare recordSize with
+	 ** PF_PAGE_SIZE and return a nonzero code if recordSize is too large for
+	 ** your RM component to handle.
+	 **/
     RC CreateFile(const char *fileName, int recordSize);
 
-	// This method should destroy the file whose name is fileName by calling PF_Manager::DestroyFile.
+	/**
+	 ** This method should destroy the file whose name is fileName by calling
+	 ** PF_Manager::DestroyFile.
+	 **/
 	RC DestroyFile(const char *fileName);
 
-	// This method should open the file called fileName by calling PF_Manager::OpenFile. If the method is successful, the fileHandle object should become a "handle" for the open RM component file. The file handle is used to manipulate the records in the file (see the RM_FileHandle class description below). As in the PF component, it should not be an error if a client opens the same RM file more than once, using a different fileHandle object each time. Each call to the OpenFile method should create a new instance of the open file. You may assume if a file has more than one opened instance then each instance of the open file may be read but will not be modified. If a file is modified while opened more than once, you need not guarantee the integrity of the file or the RM component. You may also assume that DestroyFile will never be called on an open file.
+	/** This method should open the file called fileName by calling
+	 ** PF_Manager::OpenFile. If the method is successful, the fileHandle object
+	 ** should become a "handle" for the open RM component file. The file handle
+	 ** is used to manipulate the records in the file (see the RM_FileHandle
+	 ** class description below). As in the PF component, it should not be an
+	 ** error if a client opens the same RM file more than once, using a diffe-
+	 ** rent fileHandle object each time. Each call to the OpenFile method
+	 ** should create a new instance of the open file. You may assume if a file
+	 ** has more than one opened instance then each instance of the open file
+	 ** may be read but will not be modified. If a file is modified while opened
+	 ** more than once, you need not guarantee the integrity of the file or the
+	 ** RM component. You may also assume that DestroyFile will never be called
+	 ** on an open file.
+	 **/
 	RC OpenFile(const char *fileName, RM_FileHandle &fileHandle);
 
-	// This method should close the open file instance referred to by fileHandle by calling PF_Manager:: CloseFile.
+	/** This method should close the open file instance referred to by
+	 ** fileHandle by calling PF_Manager:: CloseFile.
+	 **/
 	RC CloseFile(RM_FileHandle &fileHandle);
-
 
 private:
 	/* INTERNAL ********************* */
--- src/rm/RM_Manager.cpp
+++ src/rm/RM_Manager.cpp
@@ -6,7 +6,7 @@
 //   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2016/03/30 14:52:34 by ngoguey           #+#    #+#             //
-//   Updated: 2016/04/01 06:46:45 by ngoguey          ###   ########.fr       //
+//   Updated: 2016/04/01 07:01:51 by ngoguey          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -55,11 +55,11 @@ RC rmm::CreateFile(const char *fileName, int recordSize)
 	// check recordSize
 	if (recordSize > PF_PAGE_SIZE / MIN_RECORD_PER_PAGE)
 		return RN_RECORDPERPAGE;
-
+*/
 	// open file with PF_Manager
 	if ((err = _pfm.CreateFile(fileName)) != 0)
 		return err;
-
+/*
 	//set PF Header
 //   PF_FileHdr *hdr = (PF_FileHdr*)hdrBuf;
    hdr->firstFree = PF_PAGE_LIST_END;
