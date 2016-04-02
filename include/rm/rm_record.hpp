cd486896b7ebd13997b567d7d409088724cb7cbc
ggila <ggilaber@student.42.fr>
2016-04-02 16:52:33 +0200

    modif rm_record

--- include/rm/rm_record.hpp
+++ include/rm/rm_record.hpp
@@ -6,7 +6,7 @@
 /*   By: ggilaber <ggilaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/01 09:52:40 by ggilaber          #+#    #+#             */
-/*   Updated: 2016/04/01 15:16:16 by ggilaber         ###   ########.fr       */
+/*   Updated: 2016/04/02 16:21:12 by ggilaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,4 @@ public:
 	// Set pData to point to the record's contents
 
 	RC GetRid(RID &rid) const;       // Get the record id
-
-	RC SetRecord(char const *pageData, RID const &rid, int recordSize);
 };
--- src/rm/rm_record.cpp
+++ src/rm/rm_record.cpp
@@ -6,7 +6,7 @@
 /*   By: ggilaber <ggilaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/01 09:50:31 by ggilaber          #+#    #+#             */
-/*   Updated: 2016/04/01 15:17:41 by ggilaber         ###   ########.fr       */
+/*   Updated: 2016/04/02 16:42:59 by ggilaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,21 @@
 
 using rmr = RM_Record;
 
-/*
-**  Need those values to works:
-**  - recordsize
-**  - adresse of file buffer
-**
-**  TODO: determine whether they should be set as rmr attributes or be passed
-**        to rmr methods by caller class.
-*/
 
-
-rmr::RM_Record()
-	: _pData(NULL), _rid(-1, -1)
-{}
-
-rmr::~RM_Record()
-{
-	if (_pData != NULL)
-		delete [] _pData;
+rmr::RM_Record() : _rid(-1, -1) {
+	_pData = new char[recordSize];
 }
 
-RC rmr::SetRecord(char const *pageData, RID const &rid, int recordSize)
-{
-//	_rid = 
-	_pData = new char[recordSize];
-	(void)rid;
-	(void)pageData;
-//	memcpy();
-	return 0;
+rmr::~RM_Record() {
+	delete [] _pData;
+}
+
+RC rmr::GetData(char *&pData) const {
+	pData = _pData;
+	return pData == NULL ? RM_RECNONINIT : 0; 
+}
+
+RC GetRid(RID &rid) const {
+	pData = _pData;
+	return pData == NULL ? RM_RECNONINIT : 0; 
 }
